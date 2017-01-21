7e001852d23a5f671022b3e9a7e0a8326d720485
Benji Smith <smith.benj@husky.neu.edu>
2017-01-20 22:49:44 -0500

    Updating 3rd person camera stuff.  Now has jumping.

--- Assets/Scripts/PlayerCameraControl.cs
+++ Assets/Scripts/PlayerCameraControl.cs
@@ -12,21 +12,25 @@ public class PlayerCameraControl : MonoBehaviour {
 
 	public float playerMoveSpeed = 1.0f;
 
+	public float playerRadius = 1.0f;
+	public float playerHeight = 2.0f;
+
+	public float jumpVelocity = 6.0f;
+
 	private Vector3 followOffset;
 
-	private Vector3 lastMousePos;
+	private bool isGrounded = false;
+	private float yVelocity = 0.0f;
 
 	// Use this for initialization
 	void Start () {
 		followOffset = transform.localPosition;
 
-		lastMousePos = Input.mousePosition;
+		isGrounded = Physics.Raycast(new Ray(player.transform.position, Vector3.down), playerHeight / 2f, Physics.AllLayers);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 mouseDelta = Input.mousePosition - lastMousePos;
-		lastMousePos = Input.mousePosition;
 		float horizontal = Input.GetAxis("Mouse X") * horizontalRotateSpeed * Time.deltaTime;
 		float vertical = Input.GetAxis("Mouse Y") * verticalRotateSpeed * Time.deltaTime * -1.0f;
 
@@ -34,10 +38,34 @@ public class PlayerCameraControl : MonoBehaviour {
 
 		cameraVertical.transform.Rotate(Vector3.right, vertical);
 
-		Vector3 forwardMotion = Input.GetAxis("Vertical") * player.transform.forward * playerMoveSpeed * Time.deltaTime;
-		Vector3 sideMotion = Input.GetAxis("Horizontal") * player.transform.right * playerMoveSpeed * Time.deltaTime;
+		Vector3 forwardMotion = Input.GetAxis("Vertical") * player.transform.forward ;
+		Vector3 sideMotion = Input.GetAxis("Horizontal") * player.transform.right;
 
-		player.transform.position += (forwardMotion + sideMotion);
+		Vector3 desiredMove = forwardMotion + sideMotion;
 
+		desiredMove = desiredMove.normalized * playerMoveSpeed * Time.deltaTime;
+
+		if (isGrounded)
+		{
+			if (yVelocity <= 0.0f)
+			{
+				yVelocity = 0.0f;
+			}
+
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				yVelocity = jumpVelocity;
+			}
+		}
+		else
+		{
+			yVelocity += Physics.gravity.y * Time.deltaTime;
+		}
+
+		desiredMove.y = yVelocity * Time.deltaTime;
+
+		player.GetComponent<CharacterController>().Move(desiredMove);
+
+		isGrounded = Physics.Raycast(new Ray(player.transform.position, Vector3.down), playerHeight / 2f, Physics.AllLayers);
 	}
 }
