6caf2571cbd1675d45d3d006555f03a17e62b3a3
Benji Smith <smith.benj@husky.neu.edu>
2017-01-21 11:56:24 -0500

    Adjusting robot sight, and also adding meta files for various materials and textures.

--- Assets/Scripts/RobotCollider.cs
+++ Assets/Scripts/RobotCollider.cs
@@ -4,8 +4,47 @@ using UnityEngine;
 
 public class RobotCollider : MonoBehaviour {
 
-    void OnTriggerEnter(Collider other){
-        Debug.Log("Collision Enter");
-    }
+	public LayerMask checkSightMask;
 
+	bool playerInVision = false;
+
+	void Update()
+	{
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		if (playerInVision)
+		{
+			Vector3 target = player.transform.position + Vector3.up * (player.GetComponent<CharacterController>().height / 2);
+
+			RaycastHit info;
+			if (Physics.Linecast(transform.position, target, out info, checkSightMask, QueryTriggerInteraction.Ignore))
+			{
+				if (info.collider.gameObject == player)
+				{
+					Debug.Log("Robot saw player!");
+				}
+			}
+			else
+			{
+				Debug.Log("Robot saw player!");
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.CompareTag("Player"))
+		{
+			Debug.Log("Vision enter");
+			playerInVision = true;
+		}
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		if (col.CompareTag("Player"))
+		{
+			Debug.Log("Vision exit");
+			playerInVision = false;
+		}
+	}
 }
