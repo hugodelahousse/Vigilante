f3badcc0e550cf3a227d3c1c3d1252c98deb06e2
Benji Smith <smith.benj@husky.neu.edu>
2017-01-21 18:41:59 -0500

    Adding menu stuff.

---
+++ Assets/Scripts/MenuActions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuActions : MonoBehaviour {
+
+	public string firstLevelName = "FirstFloor";
+
+	public void RestartLevel()
+	{
+		Time.timeScale = 1.0f;
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	public void StartGame()
+	{
+		Time.timeScale = 1.0f;
+		Application.LoadLevel(firstLevelName);
+	}
+
+	public void EndGame()
+	{
+		Application.Quit();
+	}
+}
