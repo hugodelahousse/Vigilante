df6cd27f441e3053e3145cf4cb2b1f2e016c7a15
Kyle Peter Reisner <reisner.k@northeastern.edu>
2017-01-22 09:54:37 -0500

    Player annimations

--- Assets/Scripts/PlayerCameraControl.cs
+++ Assets/Scripts/PlayerCameraControl.cs
@@ -48,6 +48,8 @@ public class PlayerCameraControl : MonoBehaviour {
 
 	private GameObject menuObject;
 
+    public Animator anim;
+
 	// Use this for initialization
 	void Start () {
 		cameraOffset = transform.localPosition;
@@ -80,6 +82,8 @@ public class PlayerCameraControl : MonoBehaviour {
 			float horizontal = Input.GetAxis("Mouse X") * horizontalRotateSpeed * Time.deltaTime;
 			float vertical = Input.GetAxis("Mouse Y") * verticalRotateSpeed * Time.deltaTime * -1.0f;
 
+               
+
 			player.transform.parent.Rotate(Vector3.up, horizontal);
 
 			vertRotate += vertical;
@@ -89,7 +93,9 @@ public class PlayerCameraControl : MonoBehaviour {
 			float vertAxis = Input.GetAxis("Vertical");
 			float horizAxis = Input.GetAxis("Horizontal");
 
-			if (vertAxis < 0.0f)
+            anim.SetBool("Walking", Mathf.Abs(vertAxis) > 0.2 || Mathf.Abs(horizAxis) > 0.2);
+
+            if (vertAxis < 0.0f)
 			{
 				player.GetComponent<SpriteRenderer>().sprite = isCrouching ? backwardCrouch : backward;
 			}
@@ -105,6 +111,7 @@ public class PlayerCameraControl : MonoBehaviour {
 					isCrouching ?
 					  (isForward ? forwardCrouch : backwardCrouch)
 					: (isForward ? forward : backward);
+
 			}
 
 			{
@@ -117,8 +124,10 @@ public class PlayerCameraControl : MonoBehaviour {
 				else if (horizAxis < 0.0f)
 				{
 					player.GetComponent<SpriteRenderer>().flipX = isForward;
+                    //anim.SetBool("Facing", !isForward);
 				}
-			}
+                
+            }
 
 			Vector3 forwardMotion = vertAxis * player.transform.forward;
 			Vector3 sideMotion = horizAxis * player.transform.right;
@@ -156,6 +165,8 @@ public class PlayerCameraControl : MonoBehaviour {
 				}
 			}
 		}
+        anim.SetBool("Crouching", isCrouching);
+         
 	}
 
 	void FixedUpdate()
