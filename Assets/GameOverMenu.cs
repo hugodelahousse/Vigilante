1dfc526043bad23ba2f7c5e2ecacecd757500e2b
Hugo Delahousse <guhogu@hotmail.fr>
2017-05-09 14:43:34 -0400

    Fixed a few things

---
+++ Assets/GameOverMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().gameOverMenuObject = this.gameObject;
+        this.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
